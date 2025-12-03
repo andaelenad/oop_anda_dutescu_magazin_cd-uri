6a6d5aebc63dad6627bc1b33149b71135987946f
Anda Dutescu <andadutescu1@gmail.com>
2025-12-03 19:51:23 +0200

    update

--- scripts/run_cppcheck.sh
+++ scripts/run_cppcheck.sh
@@ -5,17 +5,17 @@ cppcheck \
   --inconclusive \
   --quiet \
   --suppress=missingIncludeSystem \
-  --exclude=catch.hpp \
-  --exclude=tests/catch.hpp \
-  --exclude=ext/catch \
-    --inline-suppr \
-    --project="${BUILD_DIR:-build}"/compile_commands.json \
-    -i"${BUILD_DIR:-build}" --suppress="*:${BUILD_DIR:-build}/*" \
-    -i"${EXT_DIR:-ext}" --suppress="*:${EXT_DIR:-ext}/*" \
-    --suppress=missingIncludeSystem \
-    --suppress=unmatchedSuppression \
-    --suppress=useStlAlgorithm \
-    --check-level=exhaustive \
-    --error-exitcode=1
+  --suppress=*:ext/catch.hpp \
+  --suppress=*:tests/catch.hpp \
+  --inline-suppr \
+  --project="${BUILD_DIR:-build}"/compile_commands.json \
+  -i"${BUILD_DIR:-build}" --suppress="*:${BUILD_DIR:-build}/*" \
+  -i"${EXT_DIR:-ext}" --suppress="*:${EXT_DIR:-ext}/*" \
+  --suppress=missingIncludeSystem \
+  --suppress=unmatchedSuppression \
+  --suppress=useStlAlgorithm \
+  --check-level=exhaustive \
+  --error-exitcode=1
+
 
 
