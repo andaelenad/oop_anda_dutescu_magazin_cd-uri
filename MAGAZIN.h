dc61bd58b47249db1aa9caa90c72b1405fb036d3
Anda Dutescu <andadutescu1@gmail.com>
2025-12-06 17:41:50 +0200

    update23

--- MAGAZIN.h
+++ MAGAZIN.h
@@ -13,27 +13,23 @@ private:
     std::string nume;
     std::vector<Comanda> comenzi;
 
-
 public:
     explicit Magazin(const std::string& nume);
-
 
     const std::string& getNume() const { return nume; }
 
     void adaugaComanda(const Comanda& c);
-
     double venitTotal() const;
     int numarComenzi() const;
 
     void sorteazaComenziDupaValoare();
-
     std::vector<Comanda> filtreazaComenziDupaArtist(const std::string& artistCautat) const;
     bool actualizeazaComanda(const Client& client, const ProdusMuzical& produs_nou);
     void raportComenziTop(int topN) const;
 
     const std::vector<Comanda>& getComenzi() const { return comenzi; }
 
-
     friend std::ostream& operator<<(std::ostream& os, const Magazin& m);
 };
-#endif
+
+#endif // MAGAZIN_H
